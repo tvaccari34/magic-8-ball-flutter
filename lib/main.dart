2f81ba6dd0fac96559391e98e7064229ca4e1e2f
Tiago Vaccari <tiago.vaccari@outlook.com>
2021-03-08 11:26:19 +0000

    Project finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything')
+          ),
+          body: Ball(),
+        )
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 4;
+  @override
+
+  Widget build(BuildContext context) {
+
+    void getRandomAnswer() {
+      setState(() {
+        ballNumber = 1 + Random().nextInt(5);
+        print('clicou no botao $ballNumber');
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                getRandomAnswer();
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
